a3a4bef98498a1ff0456141ed8f3cba5f31c5b1e
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-03-02 16:25:09 -0300

    aulas 25,26,27,28

---
+++ src/modulo_05/Precificacao/ServicoDePrecificacao.java
@@ -0,0 +1,7 @@
+package modulo_05.Precificacao;
+
+public class ServicoDePrecificacao {
+    void calcularPrecoVenda(double precoCusto){
+        precoCusto= precoCusto * 1.20;
+    }
+}
---
+++ src/modulo_05/companhia_aerea/Principal.java
@@ -0,0 +1,29 @@
+package modulo_05.companhia_aerea;
+
+public class Principal {
+    public static void main(String[] args) {
+        Aeronave aviaoGol= new Aeronave();
+        aviaoGol.totalAssentos=100;
+
+        aviaoGol.ativo=false;
+        aviaoGol.desativar();
+//        aviaoGol.ativar();
+
+        System.out.printf("Gol (%s) %d assentos disponíveis%n",
+                aviaoGol.ativo? "Ativo" : "Inativo",
+                aviaoGol.calcularAssentosDisponiveis());
+
+        Aeronave aviaoLatam= new Aeronave();
+        aviaoLatam.totalAssentos=100;
+
+//        aviaoGol.ativo=false;
+//        aviaoGol.desativar();
+        aviaoLatam.ativar();
+
+        aviaoLatam.reservarAssentos(20);
+
+        System.out.printf("Latam (%s) %d assentos disponíveis%n",
+                aviaoLatam.ativo? "Ativo" : "Inativo",
+                aviaoLatam.calcularAssentosDisponiveis());
+    }
+}
