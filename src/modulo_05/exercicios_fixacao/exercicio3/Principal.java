91d0df1f205e09d8c580d4bf204ce34eb6c253bd
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-03-06 21:58:20 -0300

    resolvendo exercicios

---
+++ src/modulo_05/exercicios_fixacao/exercicio3/Principal.java
@@ -0,0 +1,16 @@
+package modulo_05.exercicios_fixacao.exercicio3;
+/*
+Fazer um programa para receber dois números do tipo int do usuário e
+determinar se um número é permutação do outro ou não. Ex: 431 é permutação
+de 143, 42 é permutação de 204, 1211 é permutação de 1112, etc. O programa
+só deve aceitar números positivos.
+    Contagem de Dígitos: Crie dois arrays inteiros para contar os dígitos, cada um com 10 elementos (para os dígitos de 0 a 9).
+Cada posição do array representará um dígito, e o valor naquela posição representará quantas vezes aquele dígito aparece no número.
+    Preenchimento das Contagens: Itere por cada dígito dos dois números. Para cada dígito do primeiro número,
+incremente o valor na posição correspondente do primeiro array de contagem. Faça o mesmo para o segundo número com o segundo array
+de contagem.
+    Comparação das Contagens: Compare os dois arrays de contagem. Se eles são idênticos
+(ou seja, as contagens para cada dígito são as mesmas nos dois arrays), então os números são permutações um do outro.
+ */
+public class Principal {
+}
