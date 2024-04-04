c0c1cda4619b6a41a264f1a5359792ebd5f2aa4d
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-04-03 22:25:08 -0300

    resolução exercicios POO

--- src/modulo_05/exercicio5_25/Principal.java
+++ src/modulo_05/exercicio5_25/Principal.java
@@ -2,8 +2,17 @@ package modulo_05.exercicio5_25;
 
 public class Principal {
     public static void main(String[] args) {
-        FolhaPagamento Folha = new FolhaPagamento();
-        double salario = Folha.calculaSalario();
-        System.out.println("o valor do salário é " + salario);
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("João");
+        funcionario.setQuantidadeFilhos(2);
+
+        // Ajuste dos valores para double onde necessário e inclusão do Funcionario
+        ContratoTrabalho contrato = new ContratoTrabalho(40, 20, 15.5, 20.75);
+        contrato.setFuncionario(funcionario);
+
+        FolhaPagamento folha = new FolhaPagamento(contrato);
+
+        double salario = folha.calculaSalario();
+        System.out.println("O valor do salário é: " + salario);
     }
 }
