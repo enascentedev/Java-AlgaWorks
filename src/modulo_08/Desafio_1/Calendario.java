2a3c0d479a155bda9b4dd3d9c8d39732d20d919d
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-04-07 21:28:46 -0300

    desafio 2

--- src/modulo_08/Desafio_1/Calendario.java
+++ src/modulo_08/Desafio_1/Calendario.java
@@ -1,2 +1,18 @@
-package modulo_08.Desafio_1;public class Calendario {
+package modulo_08.Desafio_1;
+
+public class Calendario {
+
+    static String obterNomeMes(int numeroMes) {
+        String[] meses = {
+                "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+                "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+        };
+
+        return (numeroMes >= 1 && numeroMes <= 12) ? meses[numeroMes - 1] : null;
+    }
+
+    public static void main(String[] args) {
+        String mes = Calendario.obterNomeMes(8);
+        System.out.println(mes);
+    }
 }
--- src/modulo_08/Desafio_2/ItemCardapio.java
+++ src/modulo_08/Desafio_2/ItemCardapio.java
@@ -1,2 +1,6 @@
-package modulo_08.Desafio_2;public class ItemCardapio {
+package modulo_08.Desafio_2;
+
+public class ItemCardapio {
+    String descricao;
+    double preco;
 }
