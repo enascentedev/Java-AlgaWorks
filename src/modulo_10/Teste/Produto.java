c8d5b4242a7f649907007ef614318d443d28450d
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-04-12 20:47:28 -0300

    aulas iniciais modulo 10

--- src/modulo_10/Teste/Produto.java
+++ src/modulo_10/Teste/Produto.java
@@ -1,2 +1,25 @@
-package modulo_10.Teste;public class Produto {
+package modulo_10.Teste;
+
+public class Produto {
+
+    static final int QUANTIDADE_ESTOQUE_INICIAL=100;
+    String nome;
+    int quantidadeEstoque;
+
+    Produto() {
+        this.nome = "Sem nome";
+        this.quantidadeEstoque = QUANTIDADE_ESTOQUE_INICIAL;
+    }
+
+    Produto(String nome) {
+        this.nome = nome;
+        this.quantidadeEstoque = QUANTIDADE_ESTOQUE_INICIAL;
+    }
+
+
+    Produto(String nome, int estoqueInicial) {
+        this.nome = nome;
+        this.quantidadeEstoque = estoqueInicial;
+    }
+
 }
